6db9035ab40a04a834c0da1d58957520ec5106fb
Edmund Widl <edmund.e.widl@ieee.org>
2025-11-10 09:54:31 +0100

    Refactor code (base class, improved docstrings)

--- notebooks/knowledge_graph_adapter.py
+++ notebooks/knowledge_graph_adapter.py
@@ -3,7 +3,136 @@ from collections import defaultdict
 from typing import Any
 from SPARQLWrapper import SPARQLWrapper, JSON, POST
 
-class KnowledgeGraphAdapter:
+class KnowledgeGraphAdapterBase:
+
+    # Query template for retrieving attribute values
+    SELECT_ATTRIBUTE_VALUE = """
+        PREFIX qudt: <http://qudt.org/schema/qudt/>
+        SELECT ?value WHERE {{
+            <{urn}> qudt:value ?value .
+        }}
+        """
+
+    # Query template for retrieving attribute labels
+    SELECT_ATTRIBUTE_LABEL = """
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        SELECT ?label WHERE {{
+            <{urn}> rdfs:label ?label .
+        }}
+        """
+
+    # Query template for retrieving attribute unit
+    SELECT_ATTRIBUTE_UNIT = """
+        PREFIX dici_core: <urn:digicities:core#>
+        SELECT ?unit WHERE {{
+            {{
+                <{urn}> (dici_core:hasUnit | dici_core:hasUnit/dici_core:xUnit | dici_core:hasUnit/dici_core:yUnit) ?unit .
+            }} UNION {{
+                FILTER NOT EXISTS {{ <{urn}> dici_core:hasUnit ?any }}
+                <{urn}> a ?type .
+                ?type (dici_core:hasDefaultUnit | dici_core:hasDefaultUnit/dici_core:xUnit | dici_core:hasDefaultUnit/dici_core:yUnit) ?unit .
+            }}
+        }}
+        """
+
+    def __init__(
+            self,
+            endpoint: str
+        ) -> None:
+        self.db_connect = SPARQLWrapper(endpoint)
+        self.db_connect.setMethod(POST)
+        self.db_connect.setReturnFormat(JSON)
+
+    def retrieve_attributes(
+            self,
+            sparql_query: str
+        ) -> dict:
+        """
+        Retrieve attributes for specific entities in the knowledge graph.
+
+        The input SPARQL query should be a SELECT statement that retrieves a list of the following form:
+
+            <name entity 1> <name attribute 1.1> <urn attribute 1.1>
+            <name entity 1> <name attribute 1.2> <urn attribute 1.2>
+            ...
+            <name entity N> <name attribute N.M> <urn attribute N.M>
+
+        The output is a nested dict of the following form:
+
+            {
+            <name entity 1>: {
+                <name attribute 1.1>: {
+                        'value': <value attribute 1.1>,
+                        'unit': <unit attribute 1.1>,
+                    },
+                <name attribute 1.2>: {
+                        'value': <value attribute 1.2>,
+                        'unit': <unit attribute 1.2>,
+                    },
+                ...
+                },
+            ...
+            }
+        """
+        # Retrieve list of attributes
+        attributes = self._retrieve_from_db(sparql_query)
+
+        # Retrieve attribute values, convert list to nested dict, and return
+        return self._collect_in_dict(attributes)
+
+    def _collect_in_dict(self, attributes: list) -> dict:
+        """
+        Retrieve attribute values and convert list to nested dict
+        """
+        out = defaultdict(dict)
+        for entity, name, urn in attributes:
+            query_attribute_value = self.SELECT_ATTRIBUTE_VALUE.format(urn=urn)
+            value = self._retrieve_from_db(query_attribute_value)
+            if value:
+                query_attribute_unit = self.SELECT_ATTRIBUTE_UNIT.format(urn=urn)
+                unit = self._retrieve_from_db(query_attribute_unit)
+                if 1 == len(unit):
+                    out[entity][name] = dict(value=value[0][0], unit=unit[0][0])
+                else:
+                    out[entity][name] = dict(value=value[0][0], unit=[u[0] for u in unit if not u[0][0] == '_'])
+            else:
+                query_attribute_label = self.SELECT_ATTRIBUTE_LABEL.format(urn=urn)
+                label = self._retrieve_from_db(query_attribute_label)
+                out[entity][name] = dict(value=label[0][0])
+        return dict(out)
+
+    def _retrieve_from_db(self, query: str) -> list[tuple]:
+        """
+        Send query to graph database and retrieve results
+        """
+        self.db_connect.setQuery(query)
+        result : dict[str, dict] = self.db_connect.queryAndConvert() # type: ignore
+        vars = result['head']['vars']
+        bindings = result['results']['bindings']
+        return [tuple(KnowledgeGraphAdapter._retrieve_variable(entry, var) for var in vars) for entry in bindings]
+
+    @staticmethod
+    def _retrieve_variable(entry: dict, var: str) -> Any:
+        """
+        Helper function: retrieve variable and cast to proper type
+        """
+        data = entry[var]
+        datatype = data.get('datatype')
+        match datatype:
+            case 'http://www.w3.org/2001/XMLSchema#decimal':
+                return float(entry[var]['value'])
+            case 'https://www.w3.org/2019/wot/json-schema#Json':
+                return json.loads(entry[var]['value'])
+            case _:
+                type = data.get('type')
+                match type:
+                    case 'bnode':
+                        return '_:' + entry[var]['value']
+                    case _:
+                        return entry[var]['value']
+
+
+class KnowledgeGraphAdapter(KnowledgeGraphAdapterBase):
 
     # Query template for selecting attributes associated to windparks
     SELECT_WINDPARK_ATTRIBUTES = """
@@ -72,43 +201,11 @@ class KnowledgeGraphAdapter:
     }}
     """
 
-    # Query template for retrieving attribute values
-    SELECT_ATTRIBUTE_VALUE = """
-        PREFIX qudt: <http://qudt.org/schema/qudt/>
-        SELECT ?value WHERE {{
-            <{urn}> qudt:value ?value .
-        }}
-        """
-
-    # Query template for retrieving attribute labels
-    SELECT_ATTRIBUTE_LABEL = """
-        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-        SELECT ?label WHERE {{
-            <{urn}> rdfs:label ?label .
-        }}
-        """
-
-    # Query template for retrieving attribute unit
-    SELECT_ATTRIBUTE_UNIT = """
-        PREFIX dici_core: <urn:digicities:core#>
-        SELECT ?unit WHERE {{
-            {{
-                <{urn}> (dici_core:hasUnit | dici_core:hasUnit/dici_core:xUnit | dici_core:hasUnit/dici_core:yUnit) ?unit .
-            }} UNION {{
-                FILTER NOT EXISTS {{ <{urn}> dici_core:hasUnit ?any }}
-                <{urn}> a ?type .
-                ?type (dici_core:hasDefaultUnit | dici_core:hasDefaultUnit/dici_core:xUnit | dici_core:hasDefaultUnit/dici_core:yUnit) ?unit .
-            }}
-        }}
-        """
-
     def __init__(
             self,
             endpoint: str
         ) -> None:
-        self.db_connect = SPARQLWrapper(endpoint)
-        self.db_connect.setMethod(POST)
-        self.db_connect.setReturnFormat(JSON)
+        super().__init__(endpoint)
 
     def retrieve_windpark_info(
             self,
@@ -118,14 +215,12 @@ class KnowledgeGraphAdapter:
         """
         Retrieve all information related to the wind turbines of a specific scenario
         """
-        # Retrieve list of attributes
+        # Define SPARQL query
         query_windpark_attributes = self.SELECT_WINDPARK_ATTRIBUTES.format(
             scenario=scenario, global_wind_atlas_site=global_wind_atlas_site
             )
-        windpark_attributes = self._retrieve_from_db(query_windpark_attributes)
-
-        # Retrieve attribute values, convert list to nested dict, and return.
-        return self._collect_in_dict(windpark_attributes)
+        # Retrieve attribute values, convert list to nested dict, and return
+        return self.retrieve_attributes(query_windpark_attributes)
 
     def retrieve_turbine_info(
             self,
@@ -135,14 +230,12 @@ class KnowledgeGraphAdapter:
         """
         Retrieve all information related to the wind turbines of a specific scenario
         """
-        # Retrieve list of attributes
+        # Define SPARQL query
         query_turbine_attributes = self.SELECT_WIND_TURBINE_ATTRIBUTES.format(
             scenario=scenario, global_wind_atlas_site=global_wind_atlas_site
             )
-        turbine_attributes = self._retrieve_from_db(query_turbine_attributes)
-
-        # Retrieve attribute values, convert list to nested dict, and return.
-        return self._collect_in_dict(turbine_attributes)
+        # Retrieve attribute values, convert list to nested dict, and return
+        return self.retrieve_attributes(query_turbine_attributes)
 
     def retrieve_turbine_types(
             self,
@@ -152,62 +245,9 @@ class KnowledgeGraphAdapter:
         """
         Retrieve all information related to the wind turbines of a specific scenario
         """
-        # Retrieve list of type attributes
+        # Define SPARQL query
         query_turbine_type_attributes = self.SELECT_WIND_TURBINE_TYPE_ATTRIBUTES.format(
             scenario=scenario, global_wind_atlas_site=global_wind_atlas_site
             )
-        turbine_type_attributes = self._retrieve_from_db(query_turbine_type_attributes)
-
-        # Retrieve attribute values, convert list to nested dict, and return.
-        return self._collect_in_dict(turbine_type_attributes)
-
-    def _collect_in_dict(self, attributes: list) -> dict:
-        """
-        Retrieve attribute values and convert list to nested dict
-        """
-        out = defaultdict(dict)
-        for entity, name, urn in attributes:
-            query_attribute_value = self.SELECT_ATTRIBUTE_VALUE.format(urn=urn)
-            value = self._retrieve_from_db(query_attribute_value)
-            if value:
-                query_attribute_unit = self.SELECT_ATTRIBUTE_UNIT.format(urn=urn)
-                unit = self._retrieve_from_db(query_attribute_unit)
-                if 1 == len(unit):
-                    out[entity][name] = dict(value=value[0][0], unit=unit[0][0])
-                else:
-                    out[entity][name] = dict(value=value[0][0], unit=[u[0] for u in unit if not u[0][0] == '_'])
-            else:
-                query_attribute_label = self.SELECT_ATTRIBUTE_LABEL.format(urn=urn)
-                label = self._retrieve_from_db(query_attribute_label)
-                out[entity][name] = dict(value=label[0][0])
-        return dict(out)
-
-    def _retrieve_from_db(self, query: str) -> list[tuple]:
-        """
-        Send query to graph database and retrieve results
-        """
-        self.db_connect.setQuery(query)
-        result : dict[str, dict] = self.db_connect.queryAndConvert()
-        vars = result['head']['vars']
-        bindings = result['results']['bindings']
-        return [tuple(KnowledgeGraphAdapter._retrieve_variable(entry, var) for var in vars) for entry in bindings]
-
-    @staticmethod
-    def _retrieve_variable(entry: dict, var: str) -> Any:
-        """
-        Helper function: retrieve variable and cast to proper type
-        """
-        data = entry[var]
-        datatype = data.get('datatype')
-        match datatype:
-            case 'http://www.w3.org/2001/XMLSchema#decimal':
-                return float(entry[var]['value'])
-            case 'https://www.w3.org/2019/wot/json-schema#Json':
-                return json.loads(entry[var]['value'])
-            case _:
-                type = data.get('type')
-                match type:
-                    case 'bnode':
-                        return '_:' + entry[var]['value']
-                    case _:
-                        return entry[var]['value']
+        # Retrieve attribute values, convert list to nested dict, and return
+        return self.retrieve_attributes(query_turbine_type_attributes)
